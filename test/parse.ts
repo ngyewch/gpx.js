11e78f4645cc0d9fbc045f50eedb35b878a45ae4
Nick Ng <ngyewch@gmail.com>
2024-09-09 17:43:50 +0800

    feat: added xmldom

--- test/parse.ts
+++ test/parse.ts
@@ -1,5 +1,10 @@
 import t from 'tap';
+import { DOMParser, XMLSerializer} from '@xmldom/xmldom';
 
 t.test('parse', t => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString('<a>boo</a>', 'text/xml');
+    console.log(new XMLSerializer().serializeToString(doc));
+    
     t.end();
 });
