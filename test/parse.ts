f17ca14387df784ed1cfc0463a17d248c11dbf83
Nick Ng <ngyewch@gmail.com>
2024-09-10 21:44:53 +0800

    feat: updated test

--- test/parse.ts
+++ test/parse.ts
@@ -14,20 +14,72 @@ t.test('parse 1.0', t => {
                 t.equal(gpx.metadata.author.email, 'example@domain.com');
                 t.equal(gpx.metadata.author.link, undefined);
             }
+            t.equal(gpx.metadata.copyright, undefined);
+            if (t.not(gpx.metadata.link, undefined)) {
+                t.equal(gpx.metadata.link.href, 'https://domain.com/gpx/example');
+                t.equal(gpx.metadata.link.text, 'doc link text');
+                t.equal(gpx.metadata.link.type, undefined);
+            }
+            t.equal(gpx.metadata.time?.toISOString(), '2013-01-01T12:00:00.000Z');
+            t.equal(gpx.metadata.keywords, 'example keywords');
+            if (t.not(gpx.metadata.bounds, undefined)) {
+                t.equal(gpx.metadata.bounds.minlat, 1.2);
+                t.equal(gpx.metadata.bounds.minlon, 3.4);
+                t.equal(gpx.metadata.bounds.maxlat, 5.6);
+                t.equal(gpx.metadata.bounds.maxlon, 7.8);
+            }
         }
-        t.equal(gpx.metadata.copyright, undefined);
-        if (t.not(gpx.metadata.link, undefined)) {
-            t.equal(gpx.metadata.link.href, 'https://domain.com/gpx/example');
-            t.equal(gpx.metadata.link.text, 'doc link text');
-            t.equal(gpx.metadata.link.type, undefined);
-        }
-        t.equal(gpx.metadata.time?.toISOString(), '2013-01-01T12:00:00.000Z');
-        t.equal(gpx.metadata.keywords, 'example keywords');
-        if (t.not(gpx.metadata.bounds, undefined)) {
-            t.equal(gpx.metadata.bounds.minlat, 1.2);
-            t.equal(gpx.metadata.bounds.minlon, 3.4);
-            t.equal(gpx.metadata.bounds.maxlat, 5.6);
-            t.equal(gpx.metadata.bounds.maxlon, 7.8);
+        if (t.not(gpx.wpt, undefined)) {
+            if (t.equal(gpx.wpt.length, 2)) {
+                if (t.not(gpx.wpt[0], undefined)) {
+                    const wpt = gpx.wpt[0];
+                    t.equal(wpt.lat, 12.3);
+                    t.equal(wpt.lon, 45.6);
+                    t.equal(wpt.ele, 75.1);
+                    t.equal(wpt.magvar, 1.1);
+                    t.equal(wpt.geoidheight, 2.0);
+                    t.equal(wpt.name, 'example name');
+                    t.equal(wpt.cmt, 'example cmt');
+                    t.equal(wpt.desc, 'example desc');
+                    t.equal(wpt.src, 'example src');
+                    if (t.not(wpt.link, undefined)) {
+                        t.equal(wpt.link.href, 'http://link3');
+                        t.equal(wpt.link.text, 'link text3');
+                        t.equal(wpt.link.type, undefined);
+                    }
+                    t.equal(wpt.sym, 'example sym');
+                    t.equal(wpt.type, 'example type');
+                    t.equal(wpt.fix, '2d');
+                    t.equal(wpt.sat, 5);
+                    t.equal(wpt.hdop, 6);
+                    t.equal(wpt.vdop, 7);
+                    t.equal(wpt.pdop, 8);
+                    t.equal(wpt.ageofdgpsdata, 9);
+                    t.equal(wpt.dgpsid, 45);
+                }
+                if (t.not(gpx.wpt[1], undefined)) {
+                    const wpt = gpx.wpt[1];
+                    t.equal(wpt.lat, 13.4);
+                    t.equal(wpt.lon, 46.7);
+                    t.equal(wpt.ele, undefined);
+                    t.equal(wpt.magvar, undefined);
+                    t.equal(wpt.geoidheight, undefined);
+                    t.equal(wpt.name, undefined);
+                    t.equal(wpt.cmt, undefined);
+                    t.equal(wpt.desc, undefined);
+                    t.equal(wpt.src, undefined);
+                    t.equal(wpt.link, undefined);
+                    t.equal(wpt.sym, undefined);
+                    t.equal(wpt.type, undefined);
+                    t.equal(wpt.fix, undefined);
+                    t.equal(wpt.sat, undefined);
+                    t.equal(wpt.hdop, undefined);
+                    t.equal(wpt.vdop, undefined);
+                    t.equal(wpt.pdop, undefined);
+                    t.equal(wpt.ageofdgpsdata, undefined);
+                    t.equal(wpt.dgpsid, undefined);
+                }
+            }
         }
     }
     t.end();
@@ -66,6 +118,58 @@ t.test('parse 1.1', t => {
                 t.equal(gpx.metadata.bounds.minlon, 3.4);
                 t.equal(gpx.metadata.bounds.maxlat, 5.6);
                 t.equal(gpx.metadata.bounds.maxlon, 7.8);
+            }
+        }
+        if (t.not(gpx.wpt, undefined)) {
+            if (t.equal(gpx.wpt.length, 2)) {
+                if (t.not(gpx.wpt[0], undefined)) {
+                    const wpt = gpx.wpt[0];
+                    t.equal(wpt.lat, 12.3);
+                    t.equal(wpt.lon, 45.6);
+                    t.equal(wpt.ele, 75.1);
+                    t.equal(wpt.magvar, 1.1);
+                    t.equal(wpt.geoidheight, 2.0);
+                    t.equal(wpt.name, 'example name');
+                    t.equal(wpt.cmt, 'example cmt');
+                    t.equal(wpt.desc, 'example desc');
+                    t.equal(wpt.src, 'example src');
+                    if (t.not(wpt.link, undefined)) {
+                        t.equal(wpt.link.href, 'http://link3');
+                        t.equal(wpt.link.text, 'link text3');
+                        t.equal(wpt.link.type, 'link type3');
+                    }
+                    t.equal(wpt.sym, 'example sym');
+                    t.equal(wpt.type, 'example type');
+                    t.equal(wpt.fix, '2d');
+                    t.equal(wpt.sat, 5);
+                    t.equal(wpt.hdop, 6);
+                    t.equal(wpt.vdop, 7);
+                    t.equal(wpt.pdop, 8);
+                    t.equal(wpt.ageofdgpsdata, 9);
+                    t.equal(wpt.dgpsid, 45);
+                }
+                if (t.not(gpx.wpt[1], undefined)) {
+                    const wpt = gpx.wpt[1];
+                    t.equal(wpt.lat, 13.4);
+                    t.equal(wpt.lon, 46.7);
+                    t.equal(wpt.ele, undefined);
+                    t.equal(wpt.magvar, undefined);
+                    t.equal(wpt.geoidheight, undefined);
+                    t.equal(wpt.name, undefined);
+                    t.equal(wpt.cmt, undefined);
+                    t.equal(wpt.desc, undefined);
+                    t.equal(wpt.src, undefined);
+                    t.equal(wpt.link, undefined);
+                    t.equal(wpt.sym, undefined);
+                    t.equal(wpt.type, undefined);
+                    t.equal(wpt.fix, undefined);
+                    t.equal(wpt.sat, undefined);
+                    t.equal(wpt.hdop, undefined);
+                    t.equal(wpt.vdop, undefined);
+                    t.equal(wpt.pdop, undefined);
+                    t.equal(wpt.ageofdgpsdata, undefined);
+                    t.equal(wpt.dgpsid, undefined);
+                }
             }
         }
     }
