d8d62cf87786f3f19b4014ee7cd638b0248322b2
Nick Ng <ngyewch@gmail.com>
2024-09-11 22:56:19 +0800

    chore: updated tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,31 @@
 import {defineConfig} from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig(() => {
-    return {
-        build: {
-            minify: true,
-            sourcemap: true,
-            lib: {
-                entry: './src/index.ts',
-                name: 'gpx',
-                fileName: getFilename,
-                formats: ['es', 'umd', 'cjs'],
+export default defineConfig({
+    build: {
+        minify: true,
+        sourcemap: true,
+        lib: {
+            entry: './src/index.ts',
+            name: 'gpx',
+            fileName: (format, _): string => {
+                switch (format) {
+                    case 'es':
+                        return 'gpx.js';
+                    case 'umd':
+                        return 'gpx.umd.cjs';
+                    case 'cjs':
+                        return 'gpx.cjs';
+                    default:
+                        throw `unknown format: ${format}`;
+                }
             },
+            formats: ['es', 'umd', 'cjs'],
         },
-        plugins: [
-            dts({
-                rollupTypes: true,
-            }),
-        ],
-    };
+    },
+    plugins: [
+        dts({
+            rollupTypes: true,
+        }),
+    ],
 });
-
-function getFilename(format: string, entryName: string): string | undefined {
-    switch (format) {
-        case 'es':
-            return 'gpx.js';
-        case 'umd':
-            return 'gpx.umd.cjs';
-        case 'cjs':
-            return 'gpx.cjs';
-        default:
-            return undefined;
-    }
-}
